626d794e10455ce9ae3f1943e263f311ba1cb445
tatiana faramarzi <tfaramarzi@gmail.com>
2019-07-24 16:05:24 -0700

    Added handlers to inputs on Form.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import  React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Form from './Form.js';
 
-function App() {
+const App = () => {
 
-  const [member, setMember] = useState({});
+  const [member, setMember] = useState({name:"", email:"", role:""});
+
+  let team = [
+    {name:"Steve Zissou", email:"Steve@Life.Aquatic", role:"Oceaographer"},
+    {name:"Klaus Daimler", email:"Klaus@Life.Aquatic", role:"First Mate"},
+    {name:"Vikram Ray", email:"Vikram@Life.Aquatic", role:"Icthyologist"},
+    {name:"Vladimir Wolodarsky", email:"Vlad@Life.Aquatic", role:"Navigator"},
+    {name:"Jane Winslett-Richardson", email:"Jane@BBC.uk", role:"Journalist"}
+  ]
+
 
   return (
-   <Form />
+
+
+    <div className = "App">
+      <Form member = {member} setMember = {setMember} team={team}/>
+
+      {team.map((member) =>  <div className = "member-card">
+                              <h3>Name: {member.name}</h3>
+                              <p>Email: {member.email}</p>
+                              <p>Role: {member.role}</p>
+                            </div>)}
+    </div>  
   );
 }
 
--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,38 @@
 import React, { useState } from "react";
+import { template } from "handlebars";
 
-function Form() {
+function Form({member, setMember, team}) {
+
+    //use an object to track multiple fields:
+
+    //create an onChange handler
+    const handleChange = event => {
+        //console.log(event.target.value)
+        const updatedMember = { ...member, [event.target.name]: event.target.value };
+        setMember(updatedMember);
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        console.log("team member", member);
+        team.push(member);
+    }
 
     return (
-      <form>
-        <input /> 
-        <input type="button" value="Button" />
+      <form onSubmit={handleSubmit}>
+        <label>
+            Name
+            <input type="text" name="name" value={member.name} onChange={handleChange} required /> 
+        </label>
+        <label>
+            Email
+            <input type="email" name="email" value={member.email} onChange={handleChange}/>
+        </label>
+        <label>
+            Role
+            <input type="text" name="role" value={member.role} onChange={handleChange}/>
+        </label>
+        <button type="submit">Submit</button>
       </form>  
     );
 }
