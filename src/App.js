3b96e075cd027194d3c046cb3b7e7d63d223aa74
tatiana faramarzi <tfaramarzi@gmail.com>
2019-07-24 13:10:22 -0700

    set up project, set state in App.js, creating Form.js markup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import  React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Form from './Form.js';
+
+function App() {
+
+  const [member, setMember] = useState({});
+
+  return (
+   <Form />
+  );
+}
+
+export default App;
---
+++ src/Form.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+function Form() {
+
+    return (
+      <form>
+        <input /> 
+        <input type="button" value="Button" />
+      </form>  
+    );
+}
+
+export default Form;
